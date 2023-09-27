aec9eb24ebb2bfd7152886d6272ce176831624f0
Ali3905 <muhammadali30905@gmail.com>
2023-09-27 18:17:10 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [btn, setBtn] = useState("OFF")
+  const [btnValue, setBtnValue] = useState(false)
+  const [disable, setDisable] = useState(false)
+
+  const changeOn = async() => {
+    if (btn==="ON") {
+      setDisable(true)
+      setBtn("OFF")
+      setBtnValue(false)
+      sendChangeToDB()
+      await timeout(2000)
+      setDisable(false)
+    }else{
+      setDisable(true)
+      setBtn("ON")
+      setBtnValue(true)
+     sendChangeToDB()
+      await timeout(2000); 
+      setDisable(false)
+    }
+  }
+
+  const sendChangeToDB = async() => {
+    const res = await axios({
+      method : "post",
+      url : "http://localhost:5000/on_off",
+      data : {"on" : btnValue}
+    })
+    console.log(res);
+  }
+
+  function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeOn} disabled={disable}>
+        {btn}
+        {btnValue? "true" : "false"}
+        {disable? "true" : "false"}
+      </button>
     </div>
   );
 }
